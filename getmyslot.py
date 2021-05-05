6428d70012a0721717269bf91c1890b659b26b2c
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2021-05-05 21:26:40 +0530

    Finally working

--- getmyslot.py
+++ getmyslot.py
@@ -37,6 +37,7 @@ async def checkMentions():
                 api.update_status(reply,tweetID)
                 df = df.append({ "Username": username,"TweetID": tweetID, "Pin": pin, "Age": age},ignore_index=True)
                 df.to_csv("UserData.csv", index=False)
+                print(df.head())
                 logger.info(str(pin),str(age),username,str(tweetID))
             except:
                 reply = "@" + username + " " + 'There seems to be an error. Please make sure your tweet is in the following format ' + "errorid:" + datetime.datetime.now().strftime("%Y%m%d%H%M%S")
@@ -53,39 +54,39 @@ async def checkMentions():
 async def checkCowin():
     logger.info("Checking COWIN")
     baseURL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?"
-    df = pd.read_csv("UserData.csv")
+    coDF = pd.read_csv("UserData.csv")
     for date in dates:
-        for pin in df["Pin"].unique():
+        for pin in coDF["Pin"].unique():
 
             completeURL = baseURL+"date="+date + "&pincode=" + str(pin)
             try:
                 sessions = requests.get(completeURL).json()["sessions"]
+                if sessions:
+                    vaccines = {"COVISHIELD" : False, "COVAXIN" : False}
+                    minAge = {18:False,45:False}
+                    availableCapacity = sys.maxsize
+
+                    for session in sessions:
+                        availableCapacity = min(availableCapacity,int(session["available_capacity"]))
+                        vaccines[session["vaccine"]] = True
+                        minAge[session["min_age_limit"]] = True
+                    if minAge[18]:
+                        tweetAt(availableCapacity,vaccines,pin,date,18)
+                    if minAge[45]:
+                        tweetAt(availableCapacity,vaccines,pin,date,45)
+
+                else:
+                    continue
             except:
                 logger.info("Failed to get new sessions")
-
-            if sessions:
-                vaccines = {"COVISHIELD" : False, "COVAXIN" : False}
-                minAge = {18:False,45:False}
-                availableCapacity = sys.maxsize
-
-                for session in sessions:
-                    availableCapacity = min(availableCapacity,int(session["available_capacity"]))
-                    vaccines[session["vaccine"]] = True
-                    minAge[session["min_age_limit"]] = True
-                if minAge[18]:
-                    tweetAt(availableCapacity,vaccines,pin,date,18)
-                if minAge[45]:
-                    tweetAt(availableCapacity,vaccines,pin,date,45)
-
-            else:
-                continue
-    logger.info("Waiting...For COWIN")
-    await asyncio.sleep(2)
+            finally:
+                logger.info("Waiting...For COWIN")
+                await asyncio.sleep(3)
 
 def tweetAt(availableCapacity,vaccines,pin,date,age):
 
     reply = "Atleast " + str(availableCapacity) + " slot(s) for " + " and ".join([v for v,b in vaccines.items() if b == True]) + " is available for age " + str(age) + "+ at pin " + str(pin) + " for " + date
-    newDF = df.dropna()
+    newDF = coDF.dropna()
     tweetIDs = newDF.loc[(df["Age"]==age) & (df["Pin"]==pin),["TweetID"]]
 
 
@@ -98,12 +99,12 @@ def tweetAt(availableCapacity,vaccines,pin,date,age):
         if pd.isna(alert) or int(datetime.datetime.now().strftime("%d%H%M")) - alert > 100:
             logger.info(tweet)
             api.update_status(tweet,tweetID)
-            df.loc[df["TweetID"]==1389614929078935562,"Alert"]  = int(datetime.datetime.now().strftime("%d%H%M"))
+            coDF.loc[cpDF["TweetID"]==1389614929078935562,"Alert"]  = int(datetime.datetime.now().strftime("%d%H%M"))
 
-    df.to_csv("UserData.csv",index=False)
+    coDF.to_csv("UserData.csv",index=False)
 
 async def run():
-    await asyncio.gather(checkMentions(), checkCowin())
+    await asyncio.gather(checkMentions(),checkCowin())
     await run()
 if __name__ == "__main__":
     today = datetime.datetime.today()
